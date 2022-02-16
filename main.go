b686be8bd17e4fe2c56817d9f8e6677cca57c6f5
averyanov <averyanoff98@gmail.com>
2022-02-16 11:58:20 +0300

    Command line args

--- main.go
+++ main.go
@@ -1,28 +1,49 @@
 package main
 
 import (
+	"flag"
 	"github.com/disintegration/imaging"
 	"image/color"
 	"log"
 )
 
 func main() {
-	src, err := imaging.Open("input.png")
+	// Parsing command line args
+	var inputPath = ""
+	flag.StringVar(&inputPath, "i", "input.png", "Path to processing image")
+	var percent = 0
+	flag.IntVar(&percent, "p", 10, "Spaces length in percent from biggest dimension")
+	var outputPath = ""
+	flag.StringVar(&outputPath, "o", "output.png", "Path to save result")
+	flag.Parse()
+
+	if percent < 1 || percent > 100 {
+		log.Fatalf("incorrect percent")
+	}
+
+	// Opening src file
+	src, err := imaging.Open(inputPath)
 	if err != nil {
 		log.Fatalf("failed to open image: %v", err)
 	}
+
+	// Calculating new size
 	width := src.Bounds().Size().X
 	height := src.Bounds().Size().Y
-
+	multiplier := 1 + float64(percent)/100
 	newDim := 0
 	if height > width {
-		newDim = int(float64(height) * 1.1)
+		newDim = int(float64(height) * multiplier)
 	} else {
-		newDim = int(float64(width) * 1.1)
+		newDim = int(float64(width) * multiplier)
 	}
+
+	// Combining with white spaces
 	white := imaging.New(newDim, newDim, color.White)
 	dst := imaging.PasteCenter(white, src)
-	err = imaging.Save(dst, "output.png")
+
+	// Saving result
+	err = imaging.Save(dst, outputPath)
 	if err != nil {
 		log.Fatalf("failed to save image: %v", err)
 	}
